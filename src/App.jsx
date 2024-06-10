1316fca136c11d5f8887b2cf21b7596e08440243
Laksh-01 <lakshsachdeva01@gmail.com>
2024-06-10 23:09:56 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Board from "./TicTacToeGame/Board"
+
+import "./App.css"
+function App() {
+ 
+
+  return (
+    <>
+    <Board/>
+    </>
+  )
+}
+
+export default App
---
+++ src/TicTacToeGame/Square.jsx
@@ -0,0 +1,12 @@
+function Square(props) {
+  return (
+    <div className='square' style={{border:"1px solid" ,height:'100px', width:"100%",
+    display:"flex",
+    justifyContent:"center",
+    alignItems:"center"}} onClick={props.onClick}>
+        <h5>{props.value}</h5>
+    </div>
+  )
+}
+
+export default Square
